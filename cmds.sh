12a410f3453ec7538ae8a16634a21a25f021a134
eisnerw <eisnerw@hotmail.com>
2024-01-16 04:59:09 +0000

    Commands to load vscode, gedit

---
+++ cmds.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sudo apt-get install gedit
+sudo apt install ./Downloads/code_1.85.1-1702462158_amd64.deb
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code # or code-insiders
+sudo apt update
+sudo apt install nodejs
+npm install -g @angular/cli
+nvm install 16.20
+
+#chmod u+x cmds.sh
